75045cd7dbec32498b98a54a85ba3964ac3c1acc
cnb.bAQdnNc5AHA <ZyrpI8V3SOw95H0pDN3b4B+cnb.bAQdnNc5AHA@noreply.cnb.cool>
2026-01-07 14:57:42 +0800

    first add and run aiagent.py

---
+++ aiagent.py
@@ -0,0 +1,32 @@
+import ollama as oa
+co=oa.Client('http://localhost:11434')
+a=input()
+stream=co.chat(model='qwen3:8b',messages=[{'role':'user','content':a}],stream=True)
+m,n,reslai,reslai1=[],[],[],[]
+for c in stream:
+    m.append(c['message']['content'])
+for cc in m:
+    if cc =='':
+        pass
+    else:
+        n.append(cc)
+res=''
+for p in n:
+    res=res+p
+rres=res.split('\n')
+for z in range(len(rres)):
+    if rres[z]=='### 说明：':
+        resnum=z
+for z in range(resnum):
+    reslai.append(rres[z])
+for z in reslai:
+    if z=='':
+        pass
+    else:
+        reslai1.append(z)
+fp=open("newresult.csv","w")
+num=len(reslai1)
+for z in range(num):
+    t=reslai1[z]+'\n'
+    fp.write(t)
+fp.close()
