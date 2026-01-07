b171e6ec40b4f2dcfcb6b61bfcc0dfd9c80aa9c0
cnb.bAQdnNc5AHA <ZyrpI8V3SOw95H0pDN3b4B+cnb.bAQdnNc5AHA@noreply.cnb.cool>
2026-01-07 19:17:57 +0800

    first succeed in run aiagent.py

--- aiagent.py
+++ aiagent.py
@@ -1,6 +1,7 @@
 import ollama as oa
 co=oa.Client('http://localhost:11434')
 a=input()
+b=input("请输入文件名，推荐扩展名为csv")
 stream=co.chat(model='qwen3:8b',messages=[{'role':'user','content':a}],stream=True)
 m,n,reslai,reslai1=[],[],[],[]
 for c in stream:
@@ -14,9 +15,13 @@ res=''
 for p in n:
     res=res+p
 rres=res.split('\n')
+#for z in range(len(rres)):
+#    if rres[z]=='### 说明：':
+#        resnum=z
 for z in range(len(rres)):
-    if rres[z]=='### 说明：':
-        resnum=z
+    if rres[z]!='':
+        if rres[z][-1]=='|':
+            resnum=z
 for z in range(resnum):
     reslai.append(rres[z])
 for z in reslai:
@@ -24,7 +29,7 @@ for z in reslai:
         pass
     else:
         reslai1.append(z)
-fp=open("newresult.csv","w")
+fp=open(b,"w")
 num=len(reslai1)
 for z in range(num):
     t=reslai1[z]+'\n'
